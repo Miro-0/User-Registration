2d740afaed5faf7a7b88279dfa43e7ccaddfed5e
Miro-0 <regiagomir@gmail.com>
2026-01-14 02:39:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash
 import os
 from datetime import datetime
 
-app = Flask(__main__)
+app = Flask(__name__)
 app.secret_key = os.getenv("Repoyo")
 
 # ===================== DATABASE CONFIGURATION =====================
@@ -97,4 +97,5 @@ if __name__ == "__main__":
     
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
